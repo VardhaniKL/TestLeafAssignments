063b8b67bc6d4809a181598cc7d9f027922a652c
VardhaniKL <vardhni.karthik@gmail.com>
2025-03-03 11:51:12 +0530

    Add files via upload

---
+++ Chrome.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class Chrome 
+{
+	private static final char c = 0;
+
+	public static void main(String[] args) 
+	{
+		float version=91.0f;
+		String developer= "google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'c';
+		
+		System.out.println("Developed by  " + developer);
+		System.out.println("Version  "+ version);
+		System.out.println("Released in the year " + releaseYear);
+		System.out.println("Is it a Beta version " + isBeta);
+		System.out.println("Shortcut key is " + shortcutKey);
+	}
+
+}
